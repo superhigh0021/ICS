f74be63b57b9233fc8966f5ea5c416f94a5eb359
superhigh0021 <975440488@qq.com>
2022-02-17 00:50:11 +0800

    1

--- test.cpp
+++ test.cpp
@@ -1,4 +1,15 @@
 #include<iostream>
 using namespace std;
 
-auto func=[](int a,int b){return a<b;}
+
+int main(void){
+    union{
+        char cval;
+        int ival;
+        double dval;
+    };
+    cval='c';
+    cout<<ival<<endl;
+
+    return 0;
+}
