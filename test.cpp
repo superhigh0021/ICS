71a2ca6c117e75a8007021315699c5686dde83c1
superhigh0021 <975440488@qq.com>
2022-05-19 08:56:51 +0800

    temp

--- test.cpp
+++ test.cpp
@@ -1,19 +1,34 @@
-#include <future>
 #include <iostream>
-#include <vector>
+#include <typeinfo>
+using namespace std;
 
-class String {
+class Widget{
 public:
-    explicit String(const char* ptr){
-        std::cout<<"explicit!"<<std::endl;
+    Widget()=default;
+    Widget(int i):_i(i){
+        cout<<"cons"<<endl;
+    }
+    template<typename T>
+    Widget(T&& w):_i(std::forward<int>(w._i)){
+        cout<<"ohhhhhh"<<endl;
+        w._i=100;
     }
 
+    Widget(const Widget& w):_i(w._i){
+        cout<<"copy cons"<<endl;
+    }
+
+    void print(){
+        cout<<this->_i<<endl;
+    }
 private:
+    int _i;
 };
 
-int main(void) {
-    String s=nullptr;
-    String ss(nullptr);
+int main(int argc, char** argv) {
+    Widget w(9);
+    auto ww(w);
+    w.print();
 
     return 0;
 }
