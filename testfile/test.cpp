20f33b58b2bec82bf577c5d09f02646cda60cdce
superhigh0021 <975440488@qq.com>
2022-05-19 10:52:50 +0800

    2022.5.19

--- testfile/test.cpp
+++ testfile/test.cpp
@@ -1,14 +1,19 @@
 #include<stdio.h>
-#include<unistd.h>
+//#include<unistd.h>
 #include<string.h>
 #include<fcntl.h>
+#include<bitset>
+#include<iostream>
+
 
 #define M 5
 #define N M+M
 
 int main(){
-    int k;
-    k = N * N * 5;
-    printf("%d\n",k);
+    std::bitset<20> ak;
+    ak[0]=0;
+    std::cout<<ak[0]<<std::endl;
+
+
     return 0;
 }
