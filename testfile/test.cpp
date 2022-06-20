dcaf6ef56dd2a84f8d108912a610f2b1babd98e5
superhigh0021 <975440488@qq.com>
2022-06-20 19:35:42 +0800

    2022.6.20

--- testfile/test.cpp
+++ testfile/test.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
+#include <typeinfo>
 #include <unordered_map>
 #include <vector>
 using namespace std;
 
 class A {
 public:
-    virtual void print() {
-        cout << "base" << endl;
-    }
-};
+    explicit A(int _a)
+        : a(_a) {}
 
-class B : public A {
-public:
-    void print() {
-        cout << "derived" << endl;
-    }
+private:
+    int a;
 };
 
 int main(void) {
-    A a;
-    B b;
-    A& aa = b;
-    aa.print();
+    // string s{"123"};
+    // unordered_map<char, string>mp{{'1',"aaa"}};
+    // cout<<typeid(s[1]).name()<<endl;
+    // mp[s[1]];
+    int i = 0;
+    if (++i < 0)
+        cout << "i" << endl;
+    cout << i << endl;
+
+    return 0;
 }
