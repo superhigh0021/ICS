08fd419173c44e067e2d63ef464e0cdcb72c82fc
superhigh0021 <975440488@qq.com>
2022-06-30 18:21:41 +0800

    add project on Fano code

---
+++ InfomationTheoryProject/Fano/test.cpp
@@ -0,0 +1,165 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+struct FanoNode {
+    char ch;
+    double pro; //出现频率
+    std::string encode;
+
+    FanoNode() {}
+
+    FanoNode(const FanoNode& f1)
+        : ch(f1.ch), encode(f1.encode), pro(f1.pro) {}
+
+    FanoNode(char _ch, double _pro, std::string _str = "")
+        : ch(_ch), encode(_str), pro(_pro) {}
+};
+
+class Fano {
+private:
+    std::string sourceString;
+    std::vector<FanoNode> vec;
+    std::unordered_map<char, std::pair<int, double>> mp; //统计各字符出现次数和频率
+    std::unordered_map<char, std::string> dict;          //记录每个字符对应编码的字典
+    std::string EncodeText;
+    std::string DecodeText;
+    double Effeciency;
+
+public:
+    Fano() {}
+
+    Fano(std::string&& _str)
+        : sourceString(std::move(_str)) {
+        buildFano();
+    }
+
+    void buildFano();
+
+    void group();
+
+    void group(int lo, int hi);
+
+    void showEncodeTable(); //各字符对应的编码
+
+    void Encode(); //对输入字符串进行编码
+
+    void Decode(); //对编码序列进行解码
+
+    void calculateEfficiency(); //计算编码效率
+};
+
+void Fano::buildFano() {
+    for (auto i : sourceString)
+        ++mp[i].first;
+    for (auto i = mp.begin(); i != mp.end(); ++i) {
+        i->second.second = i->second.first / static_cast<double>(sourceString.size());
+        this->vec.emplace_back(i->first, i->second.second);
+    }
+
+    auto sortFunc = [](const FanoNode& f1, const FanoNode& f2) { return f1.pro > f2.pro; };
+    sort(this->vec.begin(), this->vec.end(), sortFunc);
+
+    // for test
+    //  for (auto i : vec) {
+    //      std::cout << "ch = " << i.ch << " pro = " << i.pro << std::endl;
+    //  }
+    this->group();
+
+    // for test
+    // for (auto i = mp.begin(); i != mp.end(); ++i) {
+    //     std::cout << "ch = " << i->first << " time = " << i->second.first
+    //               << " pro = " << i->second.second << std::endl;
+    // }
+}
+
+void Fano::group() {
+    this->group(0, vec.size() - 1);
+}
+
+void Fano::group(int lo, int hi) {
+    if (hi == lo) return;
+    int pivot = 0;      //分界点
+    double cmpFlag = 1; //因为任意频率都不可能大于1，所以初值设为1
+    for (int i = lo; i <= hi; ++i) {
+        //前半部分和后半部分的概率和
+        double frontSum = 0, backSum = 0;
+        for (int a = lo; a <= i; ++a)
+            frontSum += this->vec[a].pro;
+        for (int b = i + 1; b <= hi; ++b)
+            backSum += this->vec[b].pro;
+        if (abs(frontSum - backSum) < cmpFlag) {
+            cmpFlag = abs(frontSum - backSum);
+            pivot = i;
+        }
+    }
+    //对两个部分分别进行追加编码
+    for (int i = lo; i <= pivot; ++i)
+        this->vec[i].encode += '0';
+    for (int i = pivot + 1; i <= hi; ++i)
+        this->vec[i].encode += '1';
+
+    this->group(lo, pivot);
+    this->group(pivot + 1, hi);
+
+    // for test
+    // for (auto i : this->vec) {
+    //     std::cout << "i.ch = " << i.ch << "i.pro = " << i.pro
+    //               << "i.encode = " << i.encode << std::endl;
+    // }
+}
+
+void Fano::showEncodeTable() {
+    for (auto i : this->vec) {
+        this->dict[i.ch] = i.encode;
+        std::cout << "i.ch = " << i.ch << " i.pro = " << i.pro
+                  << " i.encode = " << i.encode << std::endl;
+    }
+}
+
+void Fano::Encode() {
+    for (auto i : sourceString)
+        EncodeText += dict[i];
+    std::cout << "the source string encode to:" << EncodeText << std::endl;
+}
+
+void Fano::Decode() {
+    std::string tmp{};
+    for (int i = 0; i < EncodeText.size(); ++i) {
+        tmp += this->EncodeText[i];
+        for (auto p : this->vec) {
+            if (p.encode == tmp) {
+                this->DecodeText += p.ch;
+                tmp = "";
+                break;
+            }
+        }
+    }
+    std::cout << "DecodeText = " << this->DecodeText << std::endl;
+}
+
+void Fano::calculateEfficiency() {
+    double entropy, aver;
+    for (auto i : this->vec) {
+        entropy -= i.pro * log2(i.pro);
+        aver += i.pro * i.encode.size();
+    }
+    this->Effeciency = entropy / aver;
+    std::cout << "the code efficiency is:" << this->Effeciency << std::endl;
+}
+
+void unitTest() {
+    Fano f("122444488888888");
+    f.showEncodeTable();
+    f.Encode();
+    f.Decode();
+    f.calculateEfficiency();
+}
+
+int main(void) {
+    unitTest();
+
+    return 0;
+}
--- testfile/testfile.cpp
+++ testfile/testfile.cpp
@@ -1,211 +1,176 @@
-#include<iostream>
-#include<string>
-#include<iomanip>
-#include<cmath>
-using namespace std;
-
-//静态三叉链表节点
-struct HNode {
-	int weight;//权重
-	int parent;//双亲指针
-	int LChild;//左孩子指针
-	int RChild;//右孩子指针
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#define Bmax 10  //最长码长度
+#define Smax 20 //数组最大长度
+struct Bit
+{
+	char b[Bmax];
+	int last;
 };
+typedef struct symbol
+{
+	char c;
+	float probability;
+	struct Bit bit;
+}sbl;
+sbl s[Smax];
 
-///编码表节点
-struct HCode {
-	char data;
-	string code;
-	float p;
-};
-
-//最小和次小选取
-void Select(HNode*& HTree, int n, int& min1, int& min2) {
-	struct TempNode {
-		int tweight;//储存权重
-		int p;//储存位置
-	};
-	TempNode* tempTree = new TempNode[n];
-	int j = 0;
-	for (int i = 0; i < n; i++) {
-		if (HTree[i].parent == -1 && HTree->weight != 0) {
-			tempTree[j].tweight = HTree[i].weight;
-			tempTree[j].p = i;
-			j++;
-		}
-	}//将HTree中无双亲的节点存储到tempTree中
-	int m1 = 0, m2 = 0;
-	for (int i = 0; i < j; i++) {
-		if (tempTree[i].tweight < tempTree[m1].tweight)//不取等号，权值相等时m1取前面的
-			m1 = i;
-	}
-	for (int i = 0; i < j; i++) {
-		if (m1 == m2)
-			m2++;//m1在位置1时，m2后移一位
-		if (tempTree[i].tweight <= tempTree[m2].tweight)//取等号，权值相等时m2取后面的
-			m2 = i;
-	}
-	min1 = tempTree[m1].p;
-	min2 = tempTree[m2].p;
-}
-
-//建立哈夫曼树
-void CreateHTree(HNode*& HTree, int weight[], int n) {
-	HTree = new HNode[2 * n - 1];
-	for (int i = 0; i < n; i++) {//根据权重初始化Huffman树
-		HTree[i].weight = weight[i];
-		HTree[i].LChild = -1;
-		HTree[i].RChild = -1;
-		HTree[i].parent = -1;
-	}
-	if (n <= 1)//错误处理
-		throw"数据不足无法建立哈夫曼树";
-
-	int min1, min2;
-	for (int i = n; i < 2 * n - 1; i++) {
-		Select(HTree, i, min1, min2);
-		HTree[min1].parent = HTree[min2].parent = i;
-		HTree[i].weight = HTree[min1].weight + HTree[min2].weight;
-		HTree[i].LChild = min1;
-		HTree[i].RChild = min2;
-		HTree[i].parent = -1;
-	}
-}
-//逆置编码函数
-void Reverse(string& s) {
-	int length = s.size();
-	char temp;
-	for (int i = 0; i < length / 2; i++) {
-		temp = s[i];
-		s[i] = s[length - i - 1];
-		s[length - i - 1] = temp;
-	}
-}
-//哈夫曼编码表
-void CreateCodeTable(HNode*& HTree, HCode*& HCodeTable, string b, int n) {
-	HCodeTable = new HCode[n];//生成编码表
-	cout << "code table is : " << endl;
-	for (int i = 0; i < n; i++) {
-		HCodeTable[i].data = b[i];
-		int child = i;
-		int parent = HTree[i].parent;
-		int k = 0;
-		while (parent != -1) {
-			if (child == HTree[parent].LChild)
-				HCodeTable[i].code += '0';
-			else
-				HCodeTable[i].code += '1';
-			child = parent;
-			parent = HTree[child].parent;
-		}
-		Reverse(HCodeTable[i].code);//逆置
-									//输出编码表
-		cout << setiosflags(ios::left) << ' ' << setw(5) << HCodeTable[i].data << ' ' << setw(5) << HCodeTable[i].code << endl;//打印编码表
-	}
-	cout << endl << endl;
-}
-//编码
-string Encode(HCode*& HCodeTable, string s, int n) {
-	string encodedString;//编码串
-	int length = s.size();
-	for (int i = 0; i < length; i++) {
-		for (int j = 0; j < n; j++) {
-			if (s[i] == HCodeTable[j].data)
-				encodedString += HCodeTable[j].code;
-		}
-	}
-	return encodedString;
-}
-//解码
-string Decode(HNode*& HTree, HCode*& HCodeTable, string s, int n) {
-	string decodedString;
-	int length = s.size();
-	for (int i = 0; i < length; i++) {
-		int parent = 2 * n - 1 - 1;//根节点在哈夫曼树中的下标
-		int j = 0;
-		while (HTree[parent].LChild != -1) {
-			if (s[i + j] == '0')
-				parent = HTree[parent].LChild;
-			else
-				parent = HTree[parent].RChild;
-			j++;
-		}
-		i += j - 1;
-		decodedString += HCodeTable[parent].data;
-	}
-	return decodedString;
-}
-//计算编码效率
-float Code_efficiency(HCode*& HCodeTable, int n) {
-	float a = 0;
-	float b = 0;
-	float c;
-	for (int i = 0; i < n; i++) {
-		a = -(HCodeTable[i].p * (log(HCodeTable[i].p) / log(2))) + a;
-	}//信息熵=-概率*log概率之和
-	for (int i = 0; i < n; i++) {
-		b = HCodeTable[i].p * HCodeTable[i].code.size() + b;
-	}//平均码长=概率*码长之和
-	c = a / b;//编码效率=信息熵/平均码长
-	return c;
-}
-//释放指针
-void Delete(HNode*& HTree, HCode*& HCodeTable) {
-
-	delete[]HTree;
-	//cout << "哈夫曼树已删除" << endl;
-	delete[]HCodeTable;
-	//cout << "哈夫曼表已删除" << endl;
-}
-
-int main() {
-	cout << "please input:" << endl;
-	char a[100];
-	cin.get(a, 100);
-	char d[50] = { '\0' };//字符
-	int w[50] = { 0 };//字符权值
-	for (int i = 0; a[i] != '\0'; i++) {//统计输入字符
-		for (int j = 0; j <= 50; j++) {
-			if (j == 50) {//若d中不存在此字符，新存入
-				int h = -1;
-				while (w[++h] != 0);//寻找存入位置
-				d[h] = a[i];
-				w[h]++;
+void sort(int n)//排序
+{
+	float t;
+	char a;
+	int i, j;
+	for (i = 1; i < n; i++)
+		for (j = 0; j < n - i; j++)
+			if (s[j].probability < s[j + 1].probability)
+			{
+				t = s[j].probability;
+				a = s[j].c;
+				s[j].probability = s[j + 1].probability;
+				s[j].c = s[j + 1].c;
+				s[j + 1].probability = t;
+				s[j + 1].c = a;
 			}
-			if (d[j] == a[i]) {
-				w[j]++;
+}
+
+void code(int low, int mid, int high)//编码
+{
+	int i;
+	for (i = low; i < high; i++)
+	{
+		if (i < mid)
+		{
+			s[i].bit.b[s[i].bit.last] = '0';
+			s[i].bit.last++;
+		}
+		else
+		{
+			s[i].bit.b[s[i].bit.last] = '1';
+			s[i].bit.last++;
+		}
+	}
+}
+
+void group1(int low, int mid, int high)//分组
+{
+	float d, dmin;
+	d = 0;
+	int i;
+	if (high == low + 1)
+		return;
+	for (i = low; i < mid; i++)
+		d += s[i].probability;
+	dmin = d - 2 * s[low].probability;
+	for (i = low + 1; i < high; i++)
+	{
+		d = fabs(dmin - 2 * s[i].probability);
+		if (d < dmin)
+			dmin = d;
+		else
+			break;
+	}
+	mid = i;
+	code(low, mid, high);
+	group1(low, mid, mid);
+	group1(mid, high, high);
+}
+
+void group(int n)
+{
+	int i, pmid, plow, phigh;
+	pmid = phigh = n;
+	plow = 0;
+	for (i = 0; i < n; i++)
+		s[i].bit.last = 0;
+	group1(plow, pmid, phigh);
+}
+
+void input(int n)
+{
+	int i;
+	char c;
+	printf("请输入符号及符号概率\n");
+	c = getchar();
+	for (i = 0; i < n; i++)
+	{
+		scanf("%c", &s[i].c);
+		scanf("%f", &s[i].probability);
+		c = getchar();
+	}
+}
+
+void output(int n)//输出编码
+{
+	int i, j;
+	printf("请输出符号,符号概率及编码\n");
+	for (i = 0; i < n; i++)
+	{
+		printf("%c\t%f\t", s[i].c, s[i].probability);
+		for (j = 0; j < s[i].bit.last; j++)
+			printf("%c", s[i].bit.b[j]);
+		printf("\n");
+	}
+}
+
+void qita(int n)
+{
+	int i, j;
+	float sum, average, effiency;
+	sum = average = 0;
+	for (i = 0; i < n; i++)
+	{
+		sum -= s[i].probability * log10(s[i].probability) / log10(2);//信源熵
+		average += s[i].probability * s[i].bit.last;//平均码长
+	}
+	effiency = sum / average;//编码效率
+	printf("信源熵:%f\n平均码长:%f\n编码效率:%f\n", sum, average, effiency);
+}
+
+
+void decode(int n, char a[100])//译码
+{
+	int i = 0, j;
+	char s2[100];
+	s2[0] = '\0';
+	while (i < strlen(a))
+	{
+		char temp[2];
+		temp[0] = a[i];
+		temp[1] = '\0';
+		strcat(s2, temp);
+		for (j = 0; j < n; j++)
+		{
+			if (strcmp(s2, s[j].bit.b) == 0)
+			{
+				printf("%c", s[j].c);
+				s2[0] = '\0';
 				break;
 			}
 		}
+		i++;
 	}
-	HNode* HTree;
-	HCode* HCodeTable;
-	string data, s;
-	int sum = 0;
-	s = a;//输入字符串转为string
-	int l = s.size();
-	int n = 0;//统计编码表中字符个数
-	while (d[n] != '\0') {
-		data += d[n];
-		n++;
-	}
-	CreateHTree(HTree, w, n);
-	CreateCodeTable(HTree, HCodeTable, data, n);
-	//求各符号概率
-	for (int j = 0; j < l; j++) {
-		sum = sum + 1;
-	}
-	for (int i = 0; i < n; i++) {
-		HCodeTable[i].p = HTree[i].weight / float(sum);
-	}
-	string encodeString;
-	encodeString = Encode(HCodeTable, s, n);
-	cout << "after coding:" << encodeString << endl;
-	cout << "input string you want to decode:";
-	char b[100];
-	cin >> b;
-	cout << "after decoding:" << Decode(HTree, HCodeTable, b, n) << endl;
-	cout << "efficiency is:" << Code_efficiency(HCodeTable, n) << endl;
-	Delete(HTree, HCodeTable);//释放指针
-	return 0;
+	printf("\n");
+}
+
+
+void main()
+{
+	int n;
+	char a[100];
+	printf("请注意当显示器再次输出“请输入符号个数时”说明输入的n值有误!\n");
+	do {
+		printf("请输入符号个数0<n<=20\n");
+		scanf("%d", &n);
+	} while (n > Smax || n <= 0);
+	input(n);
+	sort(n);
+	group(n);
+	output(n);
+	qita(n);
+	printf("请输入要译的二进制代码\n");
+	scanf("%s", a);
+	printf("输出译码结果\n");
+	decode(n, a);
 }
